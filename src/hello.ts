5065d55612ec136ef7360646da17be965259cd6f
Inrix <Inrixia@gmail.com>
2022-06-06 03:52:28 +1200

    Test interaction with button

--- src/hello.ts
+++ src/hello.ts
@@ -1,15 +1,57 @@
-import { ApplicationCommand, InteractionHandler, Interaction, InteractionResponse, InteractionResponseType } from "cloudflare-discord-bot";
+import { ApplicationCommand, InteractionHandler, InteractionResponseType } from "cloudflare-discord-bot";
+import type { EnvInterface } from ".";
+
+// import { _env } from "./";
 
 export const helloCommand: ApplicationCommand = {
 	name: "hello",
 	description: "Your bot will greet you!",
 };
-
-export const helloHandler: InteractionHandler = async (interaction: Interaction): Promise<InteractionResponse> => {
+export const helloHandler: InteractionHandler = async (interaction) => {
 	const userID = interaction.member.user.id;
 
 	return {
 		type: InteractionResponseType.ChannelMessageWithSource,
+		data: {
+			content: `Hello, <@${userID}>!`,
+			allowed_mentions: {
+				users: [userID],
+			},
+		},
+	};
+};
+
+export const listListings: ApplicationCommand = {
+	name: "listlistings",
+	description: "List current listings",
+};
+export const listListingsHandler: InteractionHandler = async (interaction, env: EnvInterface) => {
+	const listings = await env.listings.list();
+
+	return {
+		type: InteractionResponseType.ChannelMessageWithSource,
+		data: {
+			content: JSON.stringify(listings),
+			components: [
+				{
+					type: 1,
+					components: [
+						{
+							type: 2,
+							label: "Click me!",
+							style: 1,
+							custom_id: "click_one",
+						},
+					],
+				},
+			],
+		},
+	};
+};
+export const click_oneComponent: InteractionHandler = async (interaction, env: EnvInterface) => {
+	const userID = interaction.member.user.id;
+	return {
+		type: 7,
 		data: {
 			content: `Hello, <@${userID}>!`,
 			allowed_mentions: {
--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,40 @@
 import { createApplicationCommandHandler, Permissions, PermissionType } from "cloudflare-discord-bot";
 
 import { genericResponse, jsonResponse } from "@inrixia/cfworker-helpers";
-import { helloHandler, helloCommand } from "./hello";
+import { helloHandler, helloCommand, listListingsHandler, listListings, click_oneComponent } from "./hello";
 
-type EnvInterface = {
+export type EnvInterface = {
+	// Secrets
 	CLIENT_ID: string;
 	CLIENT_SECRET: string;
 	PUBLIC_KEY: string;
 	GUILD_ID: string;
+
+	// Keyvaults
+	listings: KVNamespace;
+
 	applicationCommandHandler: ReturnType<typeof createApplicationCommandHandler>;
 };
 type LRequest = Request & { env: EnvInterface };
 
+// export let _env: EnvInterface;
 const init = (env: EnvInterface) => {
 	if (env.applicationCommandHandler === undefined) {
 		env.applicationCommandHandler = createApplicationCommandHandler({
 			applicationId: env.CLIENT_ID,
 			applicationSecret: env.CLIENT_SECRET,
 			publicKey: env.PUBLIC_KEY,
-			commands: [[helloCommand, helloHandler]],
+			commands: [
+				[helloCommand, helloHandler],
+				[listListings, listListingsHandler],
+			],
+			components: {
+				click_one: click_oneComponent,
+			},
 			guildId: env.GUILD_ID,
 			permissions: new Permissions([PermissionType.SEND_MESSAGES]),
 		});
 	}
-	return env;
 };
 
 import { Router } from "itty-router";
@@ -33,13 +44,14 @@ router
 	.all("/test", (req: LRequest) => {
 		return jsonResponse(req.env);
 	})
-	.all("*", (req: LRequest) => req.env.applicationCommandHandler(req));
+	.all("*", (req: LRequest, env: EnvInterface) => env.applicationCommandHandler(req, env));
 
 export default {
 	fetch: async (req: LRequest, env: EnvInterface) => {
 		try {
-			req.env = init(env);
-			return router.handle(req);
+			init(env);
+			console.log(await req.clone().json());
+			return router.handle(req, env);
 		} catch (err) {
 			return genericResponse(500, <Error>err);
 		}
